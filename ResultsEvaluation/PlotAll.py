ef30d08728ba42def449550a17fb64409436d861
Edoardo Giacomello <edoardo.giacomello1990@gmail.com>
2018-03-22 16:42:16 +0100

    Added tex table generation based on csv test results

--- ResultsEvaluation/PlotAll.py
+++ ResultsEvaluation/PlotAll.py
@@ -1,9 +1,11 @@
 import Features
 import network_architecture as arch
+import network_architecture_WITH_FEATURE as arch_wf
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sb
+from statsmodels.sandbox.stats.multicomp import multipletests
 import os
 import csv
 import glob
@@ -109,11 +111,167 @@ def plot_all(cumulative=True):
     stat_columns = ['feature', 'W-stat', 'W-pvalue', 'T-stat', 'T-pvalue', 'U-stat', 'U-pvalue', 'KS-stat', 'KS-pvalue']
 
     for n_id, name in enumerate(names):
-        with open(output_files + "stat_test_result_{}.csv".format(name), 'w') as csvout:
+        with open(output_files + "test_uncorrected_pvalues_{}.csv".format(name), 'w') as csvout:
             writer = csv.writer(csvout, delimiter=',')
             writer.writerow(stat_columns)
             for stat in tests_results[n_id]:
                 writer.writerow(stat)
 
+def generate_latex_table(alpha, uncond_columns, correction_method='bonferroni', name_format="test_uncorrected_pvalues_{}.csv", exclude_uninformative_features=True, rejected = 'R', not_rejected = 'NR'):
+    """
+        Generate text for showing the results associated to the plotted files.
+    Loads the stat_test_result for every network and generate a table, highlighting the result if the pvalue is smaller than alpha
+    :param alpha:
+    :param uncond_columns: a list containing the names of the unconditioned networks (those without input features). The other are considered conditioned.
+    :param correction_method:
+    :param name_format:
+    :param exclude_uninformative_features:
+    :param rejected:
+    :param not_rejected:
+    :return:
+    """
+    to_exclude = [f for f in Features.features_for_evaluation if f.startswith("floors_") or f=='level_bbox_area']
+    in_folders = sorted([f + '/' for f in glob.glob(input_files + "*") if os.path.isdir(f)])
+    names = [str.split(os.path.split(f)[0], ' - ')[-1] for f in in_folders]
+    tests_results = list()
+    tests_stats = list()
 
-plot_all(cumulative=True)
+
+
+    for name in names:
+        test_result_path = output_files + name_format.format(name)
+        loaded = pd.read_csv(test_result_path, header=0, index_col=0, comment='#', delimiter=',', usecols=['feature','KS-pvalue','KS-stat'])
+        loaded.rename(columns= {'KS-pvalue': name, 'KS-stat': name+'-s'}, inplace=True)
+        tests_results.append(loaded[name])
+        tests_stats.append(loaded[name+'-s'])
+
+
+    # Merging dataframes
+    data_pv = pd.concat(tests_results, axis=1)
+    data_s = pd.concat(tests_stats, axis=1)
+    if exclude_uninformative_features:
+        data_pv = data_pv.drop(labels=to_exclude, axis=0)
+        data_s = data_s.drop(labels=to_exclude, axis=0)
+    # Calculating results, Where cond is True, keep the original value.
+
+
+    perform_test = lambda x: multipletests(x, alpha=alpha, method=correction_method)[0]
+    correct_pvalues = lambda x:  multipletests(x, alpha=alpha, method=correction_method)[1]
+
+    results = data_pv.apply(perform_test)
+    corr_pv = data_pv.apply(correct_pvalues)
+
+    # Feature clustering
+    def label_features(x, uncond_column, cond_column):
+        """
+        Compares ONE unconditional network vs ONE conditional network, assigning a label to the feature such that:
+        F1: if test is rejected in both networks
+        F2: if test is rejected in uncond and not rejected in cond
+        F3: if test is not rejected in any case
+        F4: if test is not rejected in uncond and rejected in cond
+        """
+
+        # F1: Features that are rejected in any case -> "True" in uncond and "True" in wf
+        if x[uncond_column] and x[cond_column]:
+            return "F1"
+        # F2: Features that are rejected in unconditioned and not rejected in some conditioned net -> "True" in uncond and false in wf
+        if x[uncond_column] and ~x[cond_column]:
+            return "F2"
+        # F3: Features that are not rejected in any case
+        if ~x[uncond_column] and ~x[cond_column]:
+            return "F3"
+        # F4: Features that are not rejected in unconditioned and rejected in other cases -> False in uncond, True in wf
+        if ~x[uncond_column] and x[cond_column]:
+            return "F4"
+
+    def higlight_minimum_distance(x, minimum_values, rej_value=rejected):
+        """ Puts an * on the results table if the corresponding feature belong to F1 and the stat is minimal"""
+        if (x==rej_value).all():
+            min_dist_index = minimum_values[x.name].replace('-s', '')
+            return x.where(x.index != min_dist_index, other=x+'*')
+        return x
+
+
+
+    minimum_stats = data_s.idxmin(axis=1)
+
+    # LABELING FEATURES: Generates a table that has features on the rows and couples of runs on the columns (uncond-vs-cond)
+    # For confronting the values we need to have 1 unconditioned vs 1 cond at any time.
+    # If there's only one conditioned, just use it for comparing against all the others
+
+    results_uncond = results.loc[:,uncond_columns] # equivalent to results[uncond_columns] but supports assignment
+    results_cond = results.drop(uncond_columns, axis=1)
+    if len(results_uncond.columns) == 1:
+        # Duplicating the cond column for each uncond column then dropping the original one
+        for i, uc in enumerate(results_cond.columns):
+            results_uncond.insert(loc=i+1, column='{}{}'.format(uncond_columns[0], i),
+                                  value=results_uncond[uncond_columns[0]])
+        results_uncond.drop(uncond_columns, axis=1, inplace=True)
+    if len(results_cond.columns) != len(results_uncond.columns):
+        raise ValueError("Cannot compare {} unconditioned runs with {} conditioned runs".format(len(results_cond.columns), len(results_uncond.columns)))
+    results_for_labels = pd.concat([results_uncond, results_cond], axis=1)
+
+
+    feature_labels = pd.DataFrame()
+    # Iterating over couples of matching columns
+    for uncond_col, cond_col in zip(results_uncond.columns, results_cond.columns):
+        feature_labels["{}-vs-{}".format(uncond_col, cond_col)] = results_for_labels[[uncond_col, cond_col]].apply(label_features, args=(uncond_col, cond_col), axis=1)
+    results = results.applymap(lambda x: rejected if x else not_rejected)
+    results = results.apply(higlight_minimum_distance, axis=1, args=(minimum_stats,))
+    results_input_feats = results[results.index.isin(arch_wf.features)]
+    results_other_feats = results[~results.index.isin(arch_wf.features)]
+
+    f1_features = feature_labels[(feature_labels=='F1').any(axis=1)] # Bad features
+    f2_features = feature_labels[(feature_labels=='F2').any(axis=1)] # Improved if some features are in input
+    f3_features = feature_labels[(feature_labels=='F3').any(axis=1)] # Good Features
+    f4_features = feature_labels[(feature_labels=='F4').any(axis=1)] #
+
+    print("Never working: {}".format(len(f1_features)))
+    print("Improved by inputs: {}".format(len(f2_features)))
+    print("Always working: {}".format(len(f3_features)))
+    print("Deteriorated by inputs: {}".format(len(f4_features)))
+
+
+
+    results_other_feats
+
+    with open(output_files + 'tabular_results.tex', 'w') as out:
+        caption_results_input_feats_short = "Test results for input features"
+        caption_results_input_feats = "Kolmogorov-Smirnov test results for input features, using a significance level of {} and the {} correction method. Results are indicated with R if the null hypotesis can be rejected or with NR otherwise. An astesisk indicates the network that performed better (has the minimum KS distance) if the test is rejected in every test".format(alpha, correction_method.capitalize())
+        caption_results_other_feats_short = "Test results for non input features"
+        caption_results_other_feats = "Kolmogorov-Smirnov test results for input features, using a significance level of {} and the {} correction method. Results are indicated with R if the null hypotesis can be rejected or with NR otherwise. An astesisk indicates the network that performed better (has the minimum KS distance) if the test is rejected in every test".format(alpha, correction_method.capitalize())
+        caption_data_s_short = "KS statistic values"
+        caption_data_s = "KS statistic values for the tests. The value is correlated with the distance of the cumulative distributions of the true and generated data"
+        caption_corr_pv_short = "Corrected p-values"
+        caption_corr_pv = "Corrected p-values using {} method".format(correction_method.capitalize())
+        caption_f1_features_short = "Features belonging to the F1 group"
+        caption_f1_features = "Features that belong to group F1 in at least one test. Group F1 contains the features for which the null hypotesis is rejected for both the unconditioned and conditioned network."
+        caption_f2_features_short = "Features belonging to the F2 group"
+        caption_f2_features = "Features that belong to group F2 in at least one test. Group F2 contains the features for which the null hypotesis is rejected for the unconditioned network and not rejected for the conditioned network."
+        caption_f3_features_short = "Features belonging to the F3 group"
+        caption_f3_features = "Features that belong to group F3 in at least one test. Group F3 contains the features for which the null hypotesis is not rejected for both the unconditioned and conditioned network."
+        caption_f4_features_short = "Features belonging to the F4 group"
+        caption_f4_features = "Features that belong to group F4 in at least one test. Group F4 contains the features for which the null hypotesis is not rejected for the unconditioned network and rejected for the conditioned network."
+
+        out.write(results_input_feats.to_latex(longtable=True).replace('\n', '\n \\caption[{}]{{ \\small {}}}\\\\\n'.format(caption_results_input_feats_short, caption_results_input_feats), 1))
+        out.write(results_other_feats.to_latex(longtable=True).replace('\n', '\n \\caption[{}]{{ \\small {}}}\\\\\n'.format(caption_results_other_feats_short, caption_results_other_feats), 1))
+        out.write(data_s.to_latex(longtable=True).replace('\n', '\n \\caption[{}]{{ \\small {}}}\\\\\n'.format(caption_data_s_short, caption_data_s), 1))
+        out.write(corr_pv.to_latex(longtable=True).replace('\n', '\n \\caption[{}]{{ \\small {}}}\\\\\n'.format(caption_corr_pv_short, caption_corr_pv), 1))
+        out.write(f1_features.to_latex(longtable=True).replace('\n', '\n \\caption[{}]{{ \\small {}}}\\\\\n'.format(caption_f1_features_short, caption_f1_features), 1))
+        out.write(f2_features.to_latex(longtable=True).replace('\n', '\n \\caption[{}]{{ \\small {}}}\\\\\n'.format(caption_f2_features_short, caption_f2_features), 1))
+        out.write(f3_features.to_latex(longtable=True).replace('\n', '\n \\caption[{}]{{ \\small {}}}\\\\\n'.format(caption_f3_features_short, caption_f3_features), 1))
+        out.write(f4_features.to_latex(longtable=True).replace('\n', '\n \\caption[{}]{{ \\small {}}}\\\\\n'.format(caption_f4_features_short, caption_f4_features), 1))
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    #plot_all(cumulative=True)
+    generate_latex_table(0.05, uncond_columns=['uncond'], correction_method='bonferroni' )
+
